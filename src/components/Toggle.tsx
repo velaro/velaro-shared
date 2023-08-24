36340df8afcb5c58a7ce3bdc657cc06b4d5495e5
Alix <alix@velaro.com>
2023-08-24 10:57:01 -0400

    test

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+interface Props {
+  toggled: boolean;
+  changeToggle(): void;
+}
+
+export default function Toggle(props: Props) {
+  return (
+    <div className="relative flex flex-col items-center justify-center overflow-hidden">
+      <div className="flex">
+        <label className="inline-flex relative items-center cursor-pointer">
+          <input
+            id="toggle"
+            type="checkbox"
+            className="sr-only peer"
+            checked={props.toggled}
+            readOnly
+          />
+          <div
+            id="toggle"
+            onClick={() => {
+              props.changeToggle();
+            }}
+            className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-500"
+          ></div>
+        </label>
+      </div>
+    </div>
+  );
+}
