e8199377ab613ddb7d6cac1d18e27422893a78dd
Alix <alix@velaro.com>
2023-08-24 09:42:31 -0400

    test

--- dist/cjs/components/LoadingSpinner.js
+++ dist/cjs/components/LoadingSpinner.js
@@ -1,11 +1,11 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var tslib_1 = require("tslib");
 var react_1 = tslib_1.__importDefault(require("react"));
 function LoadingSpinner() {
-    return (react_1.default.createElement("svg", { version: "1.1", xmlns: "http://www.w3.org/2000/svg", x: "0px", y: "0px", width: "40px", height: "40px", viewBox: "0 0 50 50", enableBackground: "new 0 0 50 50" },
-        react_1.default.createElement("path", { fill: "#2860a9", d: "M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z", transform: "rotate(310 25 25)" },
-            react_1.default.createElement("animateTransform", { attributeType: "xml", attributeName: "transform", type: "rotate", from: "0 25 25", to: "360 25 25", dur: "0.6s", repeatCount: "indefinite" }))));
+    return (react_1["default"].createElement("svg", { version: "1.1", xmlns: "http://www.w3.org/2000/svg", x: "0px", y: "0px", width: "40px", height: "40px", viewBox: "0 0 50 50", enableBackground: "new 0 0 50 50" },
+        react_1["default"].createElement("path", { fill: "#2860a9", d: "M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z", transform: "rotate(310 25 25)" },
+            react_1["default"].createElement("animateTransform", { attributeType: "xml", attributeName: "transform", type: "rotate", from: "0 25 25", to: "360 25 25", dur: "0.6s", repeatCount: "indefinite" }))));
 }
-exports.default = LoadingSpinner;
+exports["default"] = LoadingSpinner;
 //# sourceMappingURL=LoadingSpinner.js.map
--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -1,7 +1,7 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 exports.LoadingSpinner = void 0;
 var tslib_1 = require("tslib");
 var LoadingSpinner_1 = tslib_1.__importDefault(require("./components/LoadingSpinner"));
-exports.LoadingSpinner = LoadingSpinner_1.default;
+exports.LoadingSpinner = LoadingSpinner_1["default"];
 //# sourceMappingURL=index.js.map
