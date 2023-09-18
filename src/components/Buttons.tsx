db7e40b3401308b3a88033e095e6fe7d23f8b0d8
Joe Grise <joe@velaro.com>
2023-09-18 15:49:24 -0400

    test

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -56,6 +56,7 @@ export function PrimaryButton({
 }) {
   return (
     <button
+    style={{display: "none"}}
       className={
         "bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full text-sm font-medium"
       }
