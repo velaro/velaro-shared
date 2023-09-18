e8908234ba2827c631c48ad7fc5b74c89a2c0a21
Joe Grise <joe@velaro.com>
2023-09-18 15:51:39 -0400

    test

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -58,7 +58,7 @@ export function PrimaryButton({
     <button
     style={{display: "none"}}
       className={
-        "bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full text-sm font-medium"
+        "bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded-full text-sm font-medium"
       }
       onClick={onClick}
       disabled
