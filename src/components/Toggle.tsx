ae1cb19aae19e6a32f6036ad13b835476e514662
Jacob Troutman <jacob@velaro.com>
2023-09-22 16:28:33 -0400

    Allow toggle to be blue

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -3,9 +3,16 @@ import React from "react";
 interface Props {
   toggled: boolean;
   changeToggle(): void;
+  color?: "blue" | "green";
 }
 
 export default function Toggle(props: Props) {
+  let colorClassName =
+    "peer-focus:cornflower-blue-200 peer-checked:bg-cornflower-blue-500";
+
+  if (props.color === "green") {
+    colorClassName = "peer-focus:ring-green-300 peer-checked:bg-green-500";
+  }
   return (
     <div className="relative flex flex-col items-center justify-center overflow-hidden">
       <div className="flex">
@@ -22,7 +29,7 @@ export default function Toggle(props: Props) {
             onClick={() => {
               props.changeToggle();
             }}
-            className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-500"
+            className={`${colorClassName} w-11 h-6 bg-gray-200 rounded-full peer   peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all `}
           ></div>
         </label>
       </div>
