babfd4f7cfc056229be2dbd77e636f3560bb7f39
Jacob Troutman <jacob@velaro.com>
2023-10-13 14:44:06 -0400

    Add Form Validator

---
+++ src/components/FormValidator.tsx
@@ -0,0 +1,18 @@
+import React, { ReactElement } from "react";
+
+interface Props {
+  children: ReactElement;
+  error?: string;
+  className?: string;
+}
+
+export default function FormValidator(props: Props) {
+  return (
+    <div className={props.className}>
+      <div className={props.error ? "border border-red-500 rounded" : ""}>
+        {props.children}
+      </div>
+      {props.error && <div className="text-red-500 ">{props.error}</div>}
+    </div>
+  );
+}
