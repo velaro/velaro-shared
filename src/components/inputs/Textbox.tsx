a184eb510778a1a1b682c9e0168ea4e11f386cb6
Joe Grise <joe@velaro.com>
2023-12-07 11:17:02 -0500

    test

--- src/components/inputs/Textbox.tsx
+++ src/components/inputs/Textbox.tsx
@@ -7,20 +7,23 @@ interface Props {
   error: string;
 }
 export function Textbox({ value, onChange, disabled, error }: Props) {
-  const textColor = error ? "text-rose-500" : "text-neutral-300";
-  const borderColor = error ? "border-rose-500" : "border-neutral-300";
+  const textColor = error ? "text-rose-500" : "text-zinc-700";
+  const borderColor = error
+    ? "border-rose-500"
+    : "border-neutral-300 hover:border-gray-400";
   const bgColor = error ? "bg-pink-50" : "bg-white";
+
   return (
-    <div className="w-48 h-12 justify-start items-start inline-flex">
+    <div className="w-48 h-12 justify-start items-start inline-block">
       <div
-        className={`grow shrink basis-0 p-3 ${bgColor} rounded border ${borderColor} hover:border-gray-400 flex-col justify-center items-start gap-1 inline-flex`}
+        className={`grow shrink basis-0 p-3 ${bgColor} rounded border ${borderColor} flex-col justify-center items-start gap-1 inline-flex`}
       >
         <input
           value={value || ""}
           onChange={(e) => onChange(e.target.value)}
           disabled={disabled}
           type="text"
-          className={`${textColor} text-zinc-700 text-base font-normal font-['Rubik'] leading-normal border-none outline-none bg-inherit`}
+          className={`${textColor} text-base font-normal font-['Rubik'] leading-normal border-none outline-none bg-inherit`}
         />
       </div>
       {error && (
@@ -28,20 +31,6 @@ export function Textbox({ value, onChange, disabled, error }: Props) {
           {error}
         </div>
       )}
-    </div>
-  );
-
-  return (
-    <div className="w-48 h-12 justify-start items-start inline-flex">
-      <div className="grow shrink basis-0 p-3 bg-white rounded border border-neutral-300 hover:border-gray-400 flex-col justify-center items-start gap-1 inline-flex">
-        <input
-          value={value || ""}
-          onChange={(e) => onChange(e.target.value)}
-          disabled={disabled}
-          type="text"
-          className="text-neutral-300 hover:text-gray-400 focus:text-zinc-700 text-base font-normal font-['Rubik'] leading-normal"
-        />
-      </div>
     </div>
   );
 }
