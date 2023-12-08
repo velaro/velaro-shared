fb83ffe28057b81737ddfd64974ed87a4e3b0312
Joe Grise <joe@velaro.com>
2023-12-08 10:24:01 -0500

    update again

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -14,7 +14,7 @@ export function PrimaryButton(props: Props) {
     return (
       <Button {...props}>
         <div
-          className={`${padding} bg-neutral-300 rounded border-2 border-neutral-300 justify-center items-center gap-2 inline-flex`}
+          className={`${padding} grow shrink basis-0 bg-neutral-300 rounded border-2 border-neutral-300 justify-center items-center gap-2 inline-flex`}
         >
           <div className="text-center text-white text-sm font-semibold font-['Rubik'] leading-none">
             {props.label}
@@ -26,7 +26,7 @@ export function PrimaryButton(props: Props) {
   return (
     <Button {...props}>
       <div
-        className={`${padding} grow shrink basis-0 h-10 bg-blue-500 rounded border-2 border-blue-500 hover:bg-blue-700 hover:border-blue-700 active:bg-blue-900 active:border-blue-900 justify-center items-center gap-2 flex`}
+        className={`${padding} grow shrink basis-0 bg-blue-500 rounded border-2 maxw border-blue-500 hover:bg-blue-700 hover:border-blue-700 active:bg-blue-900 active:border-blue-900 justify-center items-center gap-2 flex`}
       >
         <div className="text-center text-white text-sm font-semibold font-['Rubik'] leading-none">
           {props.label}
--- src/components/Buttons/SecondaryButton.tsx
+++ src/components/Buttons/SecondaryButton.tsx
@@ -14,7 +14,7 @@ export function SecondaryButton(props: Props) {
     return (
       <Button {...props}>
         <div
-          className={`grow shrink basis-0 h-10 ${padding} bg-white rounded border-2 border-neutral-300 justify-center items-center gap-2 flex`}
+          className={`grow shrink basis-0 ${padding} bg-white rounded border-2 border-neutral-300 justify-center items-center gap-2 flex`}
         >
           <div className="text-center text-neutral-300 text-sm font-semibold font-['Rubik'] leading-none">
             {props.label}
@@ -27,7 +27,7 @@ export function SecondaryButton(props: Props) {
   return (
     <Button {...props}>
       <div
-        className={`grow shrink basis-0 h-10 ${padding} bg-white rounded border-2 border-blue-500 hover:border-blue-700 active:border-blue-900 justify-center items-center gap-2 flex`}
+        className={`grow shrink basis-0 ${padding} bg-white rounded border-2 border-blue-500 hover:border-blue-700 active:border-blue-900 justify-center items-center gap-2 flex`}
       >
         <div className="text-center text-blue-500 text-sm font-semibold font-['Rubik'] leading-none">
           {props.label}
