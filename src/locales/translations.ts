53f958f391131642d06d14dd292dd0db4907eabf
Jonny Warren <jonny@velaro.com>
2023-12-18 14:01:57 -0500

    another fix

---
+++ src/locales/translations.ts
@@ -0,0 +1,37 @@
+import en from "../locales/en.json"; // English
+import es from "../locales/es.json"; // Spanish
+import de from "../locales/de.json"; // German
+import fr from "../locales/fr.json"; // French
+import it from "../locales/it.json"; // Italian
+import ja from "../locales/ja.json"; // Japanese
+import ko from "../locales/ko.json"; // Korean
+import pl from "../locales/pl.json"; // Polish
+import pt_PT from "../locales/pt-pt.json"; // Portuguese (Portugal)
+import ru from "../locales/ru.json"; // Russian
+import tr from "../locales/tr.json"; // Turkish
+import zh_CN from "../locales/zh-cn.json"; // Chinese (China)
+import zh_TW from "../locales/zh-tw.json"; // Chinese (Taiwan)
+import zh from "../locales/zh.json"; // Chinese
+import en_gb from "../locales/en-gb.json"; // English (Great Britain)
+import en_us from "../locales/en-us.json"; // English (United States)
+
+const translations: any = [
+  en,
+  es,
+  de,
+  fr,
+  it,
+  ja,
+  ko,
+  pl,
+  pt_PT,
+  ru,
+  tr,
+  zh_CN,
+  zh_TW,
+  zh,
+  en_gb,
+  en_us
+];
+
+export default translations;
