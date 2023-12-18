077c4a5e07e9e8baa3cf9d3fa8506ad3a30ee321
Jonny Warren <jonny@velaro.com>
2023-12-18 12:14:01 -0500

    translations compiler

---
+++ src/locales/translations.js
@@ -0,0 +1,35 @@
+import { en } from "en.json";
+import { es } from "es.json"; // Spanish
+import { de } from "de.json"; // German
+import { fr } from "fr.json"; // French
+import { it } from "it.json"; // Italian
+import { ja } from "ja.json"; // Japanese
+import { ko } from "ko.json"; // Korean
+import { pl } from "pl.json"; // Polish
+import { pt_PT } from "pt-pt.json"; // Portuguese (Portugal)
+import { ru } from "ru.json"; // Russian
+import { zh_CN } from "zh-cn.json"; // Chinese (China)
+import { zh_TW } from "zh-tw.json"; // Chinese (Taiwan)
+import { zh } from "zh.json"; // Chinese
+import { en_gb } from "en-gb.json"; // English (Great Britain)
+import { en_us } from "en-us.json"; // English (United States)
+
+const translations = [
+  en,
+  es,
+  de,
+  fr,
+  it,
+  ja,
+  ko,
+  pl,
+  pt_PT,
+  ru,
+  zh_CN,
+  zh_TW,
+  zh,
+  en_gb,
+  en_us
+];
+
+export default translations;
