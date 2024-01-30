c46b363e7aee5fb3e672fd68501ca13a8bdc8ef7
Joe Grise <joe@velaro.com>
2024-01-30 15:20:24 -0500

    enhance

--- src/helpers/validate.ts
+++ src/helpers/validate.ts
@@ -1,11 +1,21 @@
-export default function validate(type: string, value: string) {
+export default function validate(
+  value: string,
+  type: string,
+  required: boolean
+) {
+  if (required && !value) {
+    return "A value is required";
+  }
+
   switch (type) {
     case "email":
-      return !value || /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      return (
+        !value || /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) || "Invalid email"
+      );
     case "phone":
-      return !value || /^\d{10}$/.test(value);
+      return !value || /^\d{10}$/.test(value) || "Invalid phone number";
     case "number":
-      return !value || /^\d+$/.test(value);
+      return !value || /^\d+$/.test(value) || "Invalid number";
     default:
       return true;
   }
