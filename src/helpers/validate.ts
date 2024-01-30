8bf5a173a9ecd6cac16bbe2a78a9d2032e256e26
Joe Grise <joe@velaro.com>
2024-01-30 14:39:22 -0500

    change validator to helper

---
+++ src/helpers/validate.ts
@@ -0,0 +1,12 @@
+export default function validate(type: string, value: string) {
+  switch (type) {
+    case "email":
+      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+    case "phone":
+      return /^\d{10}$/.test(value);
+    case "number":
+      return /^\d+$/.test(value);
+    default:
+      return false;
+  }
+}
