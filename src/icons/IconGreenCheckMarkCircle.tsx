fef695fc1edcc2c4f6bc865daaccdb0f60885a99
Jonny Warren <jonny@velaro.com>
2024-02-28 15:55:03 -0500

    test

--- src/icons/IconGreenCheckMarkCircle.tsx
+++ src/icons/IconGreenCheckMarkCircle.tsx
@@ -5,6 +5,8 @@ function IconCheckMarkCircle(_props: IconProps) {
   return (
     <svg fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
+        width="100%"
+        height="100%"
         d="M0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12Z"
         fill="#2FBF71"
       />
