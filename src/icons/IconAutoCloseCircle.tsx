2f249cfbd9adebdfc2a65d58a8dce981ae52be3e
Jonny Warren <jonny@velaro.com>
2024-03-14 14:45:31 -0400

    Fix for console error on AutoClose Icon

--- src/icons/IconAutoCloseCircle.tsx
+++ src/icons/IconAutoCloseCircle.tsx
@@ -18,46 +18,46 @@ function IconAutoCloseCircle(props: IconProps) {
       <path
         d="M12.1491 17.9384C15.3972 17.9384 18.0302 15.3054 18.0302 12.0573C18.0302 8.80932 15.3972 6.17627 12.1491 6.17627C8.90111 6.17627 6.26807 8.80932 6.26807 12.0573C6.26807 15.3054 8.90111 17.9384 12.1491 17.9384Z"
         stroke="white"
-        stroke-width="1.1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M15.0898 17.5298L16.2661 19.0001"
         stroke="white"
-        stroke-width="1.1"
-        stroke-linecap="round"
+        strokeWidth="1.1"
+        strokeLinecap="round"
       />
       <path
         d="M9.2085 17.5298L8.03228 19.0001"
         stroke="white"
-        stroke-width="1.1"
-        stroke-linecap="round"
+        strokeWidth="1.1"
+        strokeLinecap="round"
       />
       <path
         d="M12.1494 8.52881V12.0575L14.5018 13.2337"
         stroke="white"
-        stroke-width="1.1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M5.68018 7.64648C5.68018 7.64648 6.85639 5.88216 8.91477 5"
         stroke="white"
-        stroke-width="1.1"
-        stroke-linecap="round"
+        strokeWidth="1.1"
+        strokeLinecap="round"
       />
       <path
         d="M18.3242 7.64648C18.3242 7.64648 17.148 5.88216 15.0896 5"
         stroke="white"
-        stroke-width="1.1"
-        stroke-linecap="round"
+        strokeWidth="1.1"
+        strokeLinecap="round"
       />
       <path
         d="M12.1494 5.7002V6.0002"
         stroke="white"
-        stroke-width="1.4"
-        stroke-linecap="round"
+        strokeWidth="1.4"
+        strokeLinecap="round"
       />
     </svg>
   );
