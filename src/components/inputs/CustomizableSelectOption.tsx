bff6942fceebf2e5340ab2ef071fafb090540246
Alix <alix@velaro.com>
2024-06-20 11:31:05 -0400

    vertical alignment

--- src/components/inputs/CustomizableSelectOption.tsx
+++ src/components/inputs/CustomizableSelectOption.tsx
@@ -9,12 +9,14 @@ interface Props {
 export default function CustomizableSelectOption(props: Props) {
   return (
     <div
-      className="flex p-2 hover:bg-slate-100 rounded-md hover:text-cornflower-blue-500 hover:stroke-cornflower-blue-500"
+      className="flex items-center p-2 hover:bg-slate-100 rounded-md hover:text-cornflower-blue-500 hover:stroke-cornflower-blue-500"
       onClick={props.action}
     >
-      <div className="pr-2">{props.icon}</div>
-      <div className="">{props.text}</div>
-      <div className="pl-2 text-slate-500">{props.subtext}</div>
+      <div className="flex items-center pr-2">{props.icon}</div>
+      <div className="flex items-center">{props.text}</div>
+      <div className="flex items-center pl-2 text-slate-500">
+        {props.subtext}
+      </div>
     </div>
   );
 }
