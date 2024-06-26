bea131981a0ccc92548238dde77de8c685fe1561
Jacob Troutman <jacob@velaro.com>
2024-06-26 11:11:28 -0400

    fix search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -23,7 +23,7 @@ export default function SearchBar(props: Props) {
         ref={searchBar}
         type="text"
         id="search"
-        className="items-center w-full h-full p-4 text-sm text-gray-900 border focus:outline-none border-gray-300 rounded-lg bg-white focus:border-steel-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cornflower-blue-500 dark:focus:border-cornflower-blue-500"
+        className="items-center w-full h-full p-4 text-sm text-gray-900 border focus:outline-none border-slate-200 rounded-lg bg-white focus:border-steel-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cornflower-blue-500 dark:focus:border-cornflower-blue-500"
         placeholder={props.placeholderText}
         onChange={(e) => {
           setSearchText(e.currentTarget.value);
@@ -32,7 +32,7 @@ export default function SearchBar(props: Props) {
       ></input>
       <div
         onClick={() => props.onSearch(searchText)}
-        className="absolute inset-y-0 right-0 flex items-center pr-3"
+        className="absolute inset-y-0 right-0 flex items-center pr-3 text-slate-200"
       >
         <IconSearch />
       </div>
