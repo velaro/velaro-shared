7f0d23a6be538e5f148a386ce033f55667ed118e
Jacob Troutman <jacob@velaro.com>
2024-07-01 11:22:10 -0400

    Fix IconPlusCircle

--- src/icons/IconPlusCircle.tsx
+++ src/icons/IconPlusCircle.tsx
@@ -16,9 +16,9 @@ function IconPlusCircle(props: IconProps) {
           id="Icon"
           d="M12 8V16M8 12H16M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
     </svg>
