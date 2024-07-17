34e9528ad3727fc6c5e351142d1074fd41a65dcc
Jacob Troutman <jacob@velaro.com>
2024-07-17 10:26:07 -0400

    another badge enhancement

--- src/components/Badges/FilledBadge.tsx
+++ src/components/Badges/FilledBadge.tsx
@@ -5,16 +5,18 @@ interface Props {
   pill?: boolean;
   size?: "sm" | "md";
   backgroundColor?: string;
+  textColor?: string;
 }
 
 export default function FilledBadge(props: Props) {
   const pill = props.pill ? "rounded-full" : "rounded";
   const size = props.size === "sm" ? "text-xs" : "";
   const backgroundColor = props.backgroundColor || "bg-cornflower-blue-500";
+  const textColor = props.textColor || "text-white";
 
   return (
     <div
-      className={`${pill} ${size} ${backgroundColor} text-white px-1 py-[2px] m-1`}
+      className={`${pill} ${size} ${backgroundColor} ${textColor} px-1 py-[2px] m-1`}
     >
       {props.label}
     </div>
