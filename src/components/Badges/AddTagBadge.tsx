816182f43e7aea3710c57ce1177fb3715503b0e5
Jacob Troutman <jacob@velaro.com>
2024-10-16 17:34:06 -0400

    AddTagBadge

---
+++ src/components/Badges/AddTagBadge.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import IconCirclePlusFilled from "../../icons/IconCirclePlusFilled";
+import { badgeBgColors, BadgeColor, badgeTextColors } from "../types";
+
+interface Props {
+  label: string;
+  onClick(): void;
+  pill?: boolean;
+  size?: "sm" | "md";
+  color?: BadgeColor;
+}
+
+export default function AddTagBadge(props: Props) {
+  const pill = props.pill ? "rounded-full" : "rounded";
+  const size = props.size === "sm" ? "text-xs" : "";
+
+  const getBgColor = () => {
+    return props.color ? badgeBgColors[props.color] : badgeBgColors["blue"];
+  };
+
+  const getTextColor = () => {
+    return props.color ? badgeTextColors[props.color] : badgeTextColors["blue"];
+  };
+
+  return (
+    <span
+      className={`${pill} ${size} ${getBgColor()} text-slate-gray-800 flex items-center py-[2px] px-1 hover:cursor-pointer hover:bg-opacity-80`}
+      onClick={props.onClick}
+    >
+      <IconCirclePlusFilled className={`w-4 h-4 ${getTextColor()} mr-1`} />
+      {props.label}
+    </span>
+  );
+}
--- src/components/types.tsx
+++ src/components/types.tsx
@@ -4,3 +4,23 @@ export const tableStyles = {
 };
 
 export type TableStyle = keyof typeof tableStyles;
+
+export const badgeBgColors = {
+  red: "bg-crimson-red-100",
+  blue: "bg-cornflower-blue-100",
+  green: "bg-success-green-100",
+  yellow: "bg-sweet-orange-100",
+  purple: "bg-grape-100",
+  gray: "bg-slate-100"
+};
+
+export type BadgeColor = keyof typeof badgeBgColors;
+
+export const badgeTextColors = {
+  red: "text-crimson-red-300",
+  blue: "text-cornflower-blue-300",
+  green: "text-success-green-300",
+  yellow: "text-sweet-orange-300",
+  purple: "text-grape-300",
+  gray: "text-slate-300"
+};
